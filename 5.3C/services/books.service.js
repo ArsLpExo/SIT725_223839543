7d8f419a1d73dedc44718bb35ed55e110149741c
a_billion_mandras <amodarasangisenarath@gmail.com>
2026-06-03 15:34:00 +1000

    fix(server): convert Decimal128 price values to string before sending response

--- 5.3C/services/books.service.js
+++ 5.3C/services/books.service.js
@@ -1,4 +1,38 @@
+// const Book = require('../models/book.model');
+
+// exports.getAllBooks = () => Book.find({});
+// exports.getBookById = (id) => Book.findById(id);
+
 const Book = require('../models/book.model');
 
-exports.getAllBooks = () => Book.find({});
-exports.getBookById = (id) => Book.findById(id);
+exports.getAllBooks = async () => {
+  const books = await Book.find({});
+
+  return books.map(book => ({
+    _id: book._id,
+    title: book.title,
+    author: book.author,
+    year: book.year,
+    genre: book.genre,
+    summary: book.summary,
+    image: book.image,
+    price: book.price.toString()   // <-- FIX
+  }));
+};
+
+exports.getBookById = async (id) => {
+  const book = await Book.findById(id);
+
+  if (!book) return null;
+
+  return {
+    _id: book._id,
+    title: book.title,
+    author: book.author,
+    year: book.year,
+    genre: book.genre,
+    summary: book.summary,
+    image: book.image,
+    price: book.price.toString()   // <-- FIX
+  };
+};
